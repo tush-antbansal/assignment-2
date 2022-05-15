34a1d49bdbf9d85ca1f9cb23c5b3f833b1db1a86
tush-antbansal <101082652+tush-antbansal@users.noreply.github.com>
2022-05-15 13:01:41 +0530

    Add files via upload

---
+++ assignment2sol.py
@@ -0,0 +1,53 @@
+Python 3.10.4 (tags/v3.10.4:9d38120, Mar 23 2022, 23:13:41) [MSC v.1929 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+
+================ RESTART: C:\Users\lenovo\Desktop\assignment2.py ===============
+-----------QUESTION 1-------------------
+----------Part a-------------------------
+35
+----------Part b-------------------------
+egaugnal evitisnes esac a si nohtyP
+----------Part c-------------------------
+a case sensitive
+----------Part d-------------------------
+Python is object oriented language
+----------Part e-------------------------
+10
+----------Part f-------------------------
+Pythonisacasesensitivelanguage
+
+-----------QUESTION 2-------------------
+Enter your name: Tushant Bansal
+Enter Your SID: 21102084
+Enter your department: Civil
+Enter your CGPA: 9.0
+Hey, Tushant Bansal Here! 
+My SID is 21102084
+I am from Civil department and my cg is 9.0
+
+-----------QUESTION 3-------------------
+----------Part a-------------------------
+8
+----------Part b-------------------------
+58
+----------Part c-------------------------
+50
+----------Part d-------------------------
+224 40
+----------Part e-------------------------
+14 0
+
+-----------QUESTION 4-------------------
+Enter the string: My name is Tushant
+YES
+
+-----------QUESTION 5-------------------
+Enter side 1: 7
+Enter side 2: 12
+Enter side 3: 7
+NO
+
+-----------QUESTION 6-------------------
+Enter first number:10
+Enter second number:4
+3
